38c0f84fd00d5549ee1d124840231077b5f73a9a
daichi <badominton10@hotmail.co.jp>
2010-10-04 17:20:16 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ZantareDaichi::Application.config.secret_token = 'b3139aa4fbbb94dcd292399c27667661410350803b0711744454ac50f2f994c7e7780c76afa4dcce97c52cc9de95487b90ba50a7a6d1c9d338570e459b1454ef'
